b60b657bfa04a3686d144712707dbdcb638ac0a8
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-02-23 15:50:18 +0800

    Refactor NavMenu style component with mobile first design principles

--- src/design/NavMenu.js
+++ src/design/NavMenu.js
@@ -2,46 +2,51 @@ import styled from "styled-components";
 import theme from '../design/theme';
 
 const NavMenu = styled.nav`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  @media ${theme.breakpoints[1]} {
     display: flex;
     flex-direction: row;
     justify-content: space-evenly;
     align-items: baseline;
     margin-bottom: 10px;
+  }
 
-    @media ${theme.breakpoints[0]} {
-          border: 1px solid magenta;
-          display: flex;
-          flex-direction: column-reverse;
-          justify-content: center;
-          align-items: center;
-    }
-
-    a {
+  a {
+    width: 100%;
+    margin: 0 2.78vw;
+    padding: 20px 10px;
+    border-bottom: 1px solid ${theme.color.primaryDark};
+    text-align: center;
+    text-decoration: none;
+    font-size: ${theme.fontSizing.s};
+    font-weight: 600;
+    line-height: 2.6rem;
+    letter-spacing: 0.2rem;
+    color: ${theme.colorScheme.headerFontColor};
+    white-space: nowrap;
+    
+    @media ${theme.breakpoints[1]} {
       width: auto;
       margin: 0 1.39vw;
       padding: 12px;
-      text-align: center;
-      text-decoration: none;
-      font-size: ${theme.fontSizing.s};
-      font-weight: 600;
-      line-height: 2.6rem;
-      letter-spacing: 0.2rem;
-      color: ${theme.colorScheme.headerFontColor};
-      white-space: nowrap;
-      
-      @media ${theme.breakpoints[0]} {
-        border: 1px solid red;
-        width: 100%;
-        margin: 0 2.78vw;
-        padding: 20px 10px;
-        border-bottom: 1px solid ${theme.color.primaryDark};
-      }
-
-      &:hover {
-        color: ${theme.colorScheme.headerBgColor};
-        background: ${theme.colorScheme.headerFontColor};
-      }
+      border-bottom: initial;
     }
+
+    &:hover {
+      color: ${theme.colorScheme.headerBgColor};
+      background: ${theme.colorScheme.headerFontColor};
+    }
+
+    &:first-child {
+      border-top: 1px solid ${theme.color.primaryDark};
+      @media ${theme.breakpoints[1]} {
+        border: none;
+      }
+    }  
   }
 `
 

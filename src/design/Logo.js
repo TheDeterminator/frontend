d57337ed6aa5d10154ff77006e6f03424ac246b6
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-02-23 14:28:47 +0800

    Update Logo component

--- src/design/Logo.js
+++ src/design/Logo.js
@@ -5,6 +5,7 @@ const Logo = styled.h1`
   font-family: ${theme.fontStyles.logoFont};
   font-size: ${theme.fontSizing.xl};
   color: ${theme.colorScheme.headerFontColor};
+  padding: 10px 0;
 `
 
 export default Logo;
